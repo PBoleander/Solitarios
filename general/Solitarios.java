19b7b23e3b4d90a331756c723b514a617c384fa1
Pascual <48403430+PBoleander@users.noreply.github.com>
2020-12-12 22:20:19 +0100

    Sustituido el CardLayout del JFrame por un JTabbedPane

--- general/Solitarios.java
+++ general/Solitarios.java
@@ -1,44 +1,29 @@
 package general;
 
-import juegos.solitarios.Solitario;
 import juegos.solitarios.SolitarioA;
 import juegos.solitarios.SolitarioB;
 
 import javax.swing.*;
-import java.awt.*;
 
 class Solitarios extends JFrame {
-
-    private final Solitario solitarioA, solitarioB;
 
     Solitarios() {
         super();
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("Solitarios");
-        setLayout(new CardLayout());
         setResizable(false);
 
-        this.solitarioA = new SolitarioA();
-        this.solitarioB = new SolitarioB();
+        JTabbedPane tabbedPane = new JTabbedPane();
 
-        add(solitarioA);
-        add(solitarioB);
-        setJMenuBar(new Menu(this));
+        tabbedPane.addTab("Solitario A", new SolitarioA());
+        tabbedPane.addTab("Solitario B", new SolitarioB());
+
+        add(tabbedPane);
 
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-    }
-
-    void mostrarA() {
-        solitarioA.setVisible(true);
-        solitarioB.setVisible(false);
-    }
-
-    void mostrarB() {
-        solitarioB.setVisible(true);
-        solitarioA.setVisible(false);
     }
 
     public static void main(String[] args) {
